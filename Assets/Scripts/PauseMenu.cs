8a90918359060cfb53dbf1943858583b66f0167a
Kirsty Tschirpig <1004120@student.sae.edu.au>
2017-07-13 00:09:01 +1000

    Adding in some menu stuff

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused;
+
+    [SerializeField]
+    private GameObject pausePanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+
+            if (isPaused)
+            {
+                pausePanel.SetActive(true);
+                Time.timeScale = 0;
+            }
+
+            if (!isPaused)
+            {
+                pausePanel.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    public void GameScene()
+    {
+        SceneManager.LoadScene("__MAIN__");
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("__MENU__");
+    }
+
+    public void OptionsScene()
+    {
+        SceneManager.LoadScene("__OPTIONS__");
+    }
+
+    public void CreditsScene()
+    {
+        SceneManager.LoadScene("__CREDITS__");
+    }
+
+    public void Exit()
+    {
+
+    }
+}
