ef5724782f941680a406cc975848a5c9e7dbbef6
SAE\ktschirpig <1004120@student.sae.edu.au>
2017-07-18 11:30:02 +1000

    More save state things

--- Assets/Scripts/MicStart.cs
+++ Assets/Scripts/MicStart.cs
@@ -8,6 +8,10 @@ public class MicStart : MonoBehaviour
     public GameObject signalObjectPrefab;
     public GameObject signalObject;
 
+    //Can be used by gameobject to check bools
+    public GameObject currentNode;
+    public GameObject previousNode;
+
     private GameObject signal;
     public Transform nodePoweredTransform;
 
@@ -19,16 +23,26 @@ public class MicStart : MonoBehaviour
         signalObject = signal;
 
         signalObject.GetComponent<SignalFlowHolder>().signalFlowObjectType = this.GetComponent<Node>().signalObject;
+
+        currentNode = this.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (this.GetComponent<Node>().powering != null)
+		if (currentNode.GetComponent<Node>().powering != null)
         {
             Debug.Log("powering");
-            nodePoweredTransform = this.GetComponent<Node>().powering.transform;
+            nodePoweredTransform = currentNode.GetComponent<Node>().powering.transform;
             signalObject.transform.position = nodePoweredTransform.position;
+
+            GameObject currentNodeHolder = currentNode.GetComponent<Node>().powering;
+            currentNode = currentNodeHolder;
+            previousNode = currentNode.GetComponent<Node>().receiving;
         }
+
+        //Check to make sure previous and current node are still connected
+
+        //Set up for multiple inputs
 	}
 }
