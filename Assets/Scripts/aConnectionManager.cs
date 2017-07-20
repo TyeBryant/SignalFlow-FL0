1af3d5ea94af04af2c6acb5b8155c86a7be1d809
Unknown <1005732@Student.sae.edu.au>
2017-07-20 20:36:38 +1000

    Script cleanup
    I'm testing out cleanin up the Node script because it's most likely not going to work out for anyone in the long run

---
+++ Assets/Scripts/aConnectionManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aConnectionManager : MonoBehaviour {
+
+    [HideInInspector]
+    public bool isCarryingSignal = false;
+    public GameObject inputFrom;
+    public GameObject mousePointer;
+
+    Camera camera;
+    Vector3 mousePosition = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+        //Retrieve the camera
+        if (!GetComponent<Camera>())
+            Debug.LogError("The connection manager script is not connected to the Main Camera!");
+        else
+            camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isCarryingSignal)
+        {
+            //If the player right clicks, disconnect from the signal
+            if (Input.GetMouseButtonDown(1))
+                DisconnectSignal();
+        }
+	}
+
+    //Disconnect the signal from the mouse pointer
+    public void DisconnectSignal()
+    {
+        //Change signal to false and make input object null
+        isCarryingSignal = false;
+        inputFrom = null;
+    }
+
+    //Carry the signal from an object
+    public void CarrySignal(GameObject _inputFrom)
+    {
+        inputFrom = _inputFrom;
+        isCarryingSignal = true;
+    }
+
+    //Place the signal down on the outputting node
+    public void PlaceSignal(GameObject _outputTo)
+    {
+        _outputTo.GetComponent<aNode>().inputs.Add(inputFrom);
+        inputFrom.GetComponent<aNode>().outputs.Add(_outputTo);
+    }
+}
---
+++ Assets/Scripts/aNode.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aNode : MonoBehaviour {
+
+    /// <summary>
+    /// This is a new Node script is intended to be a parent script
+    /// for every nodetype possible.
+    /// 
+    /// This script should be able to:
+    /// - Define what type of object this node is
+    /// - Define what signals this node is a part of
+    /// - Accept signal from specific types of nodes
+    /// - Connect to those nodes on click
+    /// - Handle both multiple inputs and outputs
+    /// </summary>
+    /// 
+
+    //An enum used to define the type of node that this is
+    public enum Type
+    {
+        ET_NULL,
+        ET_MICROPHONE,
+        ET_JUNCTIONBOX,
+        ET_DIRECTOUT,
+        ET_PREAMP,
+        ET_CHANNELFADER,
+        ET_MASTERFADER,
+        ET_DAW,
+        ET_PATCHBAY,
+        ET_CHANNELS,
+        ET_AUXSENDS,
+        ET_AUXMASTERS,
+        ET_MONITOR,
+        ET_HEADPHONES,
+        ET_HEADPHONEAMP,
+        ET_OTHERPATCHBAYNODES,
+        ET_OUTPUT,
+        ET_INPUT,
+        ET_MIXB,
+        ET_MIXBMAINMIX,
+        ET_MIXBTOMAINMIX,
+        ET_MAINMIX,
+        ET_DEAD
+    }
+
+    //An enum used to determine what channel I am a part of
+    public enum Channel
+    {
+        EC_NULL,
+        EC_ONE,
+        EC_TWO,
+        EC_THREE,
+        EC_FOUR,
+        EC_FIVE,
+        EC_MULTI
+    }
+
+    [Tooltip("Determines the type of this node")]
+    public Type nodeType;
+
+    [Tooltip("Determines what types I can accept a signal from")]
+    public Type[] acceptableTypes;
+
+    [Tooltip("Determines the channel of this node")]
+    public Channel nodeChannel;
+
+    public int maximumInputs = 1, maximumOutputs = 1;
+
+    [HideInInspector]
+    public List<GameObject> inputs = new List<GameObject>(), outputs = new List<GameObject>();
+
+    [HideInInspector]
+    public bool isPowered;
+
+    [HideInInspector]
+    aConnectionManager connectionManager;
+
+	// Use this for initialization
+	void Start () {
+
+        ///Errors and Warnings
+        //If the node type is not define, throw an error
+        if (nodeType == Type.ET_NULL)
+            Debug.LogError(this.gameObject.name + " Does not have a defined nodeType!");
+        //If the node isn't a microphone and there's a defined channel
+        else if (nodeType != Type.ET_MICROPHONE && nodeChannel != Channel.EC_NULL)
+            nodeChannel = Channel.EC_NULL;
+
+        //If the node isn't a microphone and there isn't an acceptable type
+        if (nodeType != Type.ET_MICROPHONE && acceptableTypes.Length == 0)
+            Debug.LogError(this.gameObject.name + " does not hold a list of acceptable types");
+
+        //If there's a connection manager
+        if (FindObjectOfType<aConnectionManager>())
+            connectionManager = FindObjectOfType<aConnectionManager>();
+        //If there's no connection manager
+        else
+            Debug.LogError("There is no connection manager present on the scene");
+
+        if (maximumInputs == 0 || maximumOutputs == 0)
+            Debug.LogError(this.gameObject.name + " has no designated number of maximum outputs or inputs");
+        
+        ///Initialisations        
+        //If the node is a microphone
+        if (nodeType == Type.ET_MICROPHONE)
+            isPowered = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //When the sprite has been moused over
+    public void OnMouseOver()
+    {
+        //When the player clicks
+        if (Input.GetMouseButtonDown(0))
+        {
+            //if this node is powered and the connection manager is carrying a signal
+            if (isPowered && !connectionManager.isCarryingSignal)
+            {
+                //  Manage picking up the signal
+                if(outputs.Count < maximumOutputs)
+                    connectionManager.CarrySignal(this.gameObject);
+            }else if(!isPowered && connectionManager.isCarryingSignal)
+            {
+                //If check returns true, then run place signal on the connection amanger
+                if (Check())
+                    connectionManager.PlaceSignal(this.gameObject);
+            } else if(isPowered && connectionManager.isCarryingSignal)
+            {
+                //If check returns true and I can still accept inputs
+                if (Check() && inputs.Count < maximumInputs)
+                    connectionManager.PlaceSignal(this.gameObject);
+                else
+                {
+                    Disconnect();
+                    connectionManager.PlaceSignal(this.gameObject);
+                }
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+            Disconnect();
+    }
+
+    public void Disconnect()
+    {
+        //For all object's getting a signal from me
+        foreach(GameObject output in outputs)
+        {
+            aNode outN = output.GetComponent<aNode>();
+
+            //Remove this node from it's input list
+            outN.inputs.Remove(this.gameObject);
+
+            //If this node is providing power to others
+            if (outN.outputs.Count > 0)
+                outN.Disconnect();
+
+            //If there is no more inputs from anything,this node is no longer powered
+            if (outN.inputs.Count == 0)
+                outN.isPowered = false;
+
+            //Remove the object from the outputs list
+            outputs.Remove(output);
+        }
+    }
+
+    //Checks whether or not a connection can be established
+    public bool Check()
+    {
+        bool ret = false;
+
+        //Search through all types
+        foreach (Type t in acceptableTypes)
+        {
+            //If the type is the same as accepted turn ret to true
+            if (t == connectionManager.inputFrom.GetComponent<aNode>().nodeType)
+                ret = true;
+        }
+        return ret;
+    }
+}
