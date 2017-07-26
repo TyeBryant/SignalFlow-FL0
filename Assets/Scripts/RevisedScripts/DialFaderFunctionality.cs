b22342deb3e50e3bbddde0b9df94dda247954803
Kirsty Tschirpig <1004120@student.sae.edu.au>
2017-07-27 01:10:50 +1000

    dial fader functionality and stuff

---
+++ Assets/Scripts/RevisedScripts/DialFaderFunctionality.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using gameManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using Dial;
+
+public class DialFaderFunctionality : MonoBehaviour
+{
+    public bool df_Select = false;
+
+    public GameObject df_Holder;
+    public GameObject dial;
+    public GameObject fader;
+
+    public float dValue;
+    public float fValue;
+
+    private GameObject gameManager;
+
+    private GameObject thisObject;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+
+        thisObject = this.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (dValue == 0 && fValue == 0)
+        {
+            thisObject.GetComponent<aNode>().enabled = false;
+        }
+
+        if (dValue > 0 && fValue > 0)
+        {
+            thisObject.GetComponent<aNode>().enabled = true;
+        }
+
+        if (dial != null)
+        {
+            if (df_Select == true)
+            {
+                df_Holder.SetActive(true);
+            }
+
+            if (df_Select == false)
+            {
+                df_Holder.SetActive(false);
+            }
+
+            if (gameManager.GetComponent<GameManager>().gamePaused == true)
+            {
+                df_Select = false;
+            }
+        }
+
+        if (dial == null)
+        {
+            dValue = 1;
+        }
+        else
+        {
+            dValue = dial.GetComponent<DialRotation>().dialValue;
+        }
+
+        if (fader == null)
+        {
+            fValue = 1;
+        }
+        else
+        {
+            fValue = fader.GetComponent<Slider>().value;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && gameManager.GetComponent<GameManager>().gamePaused == false)
+        {
+            df_Select = !df_Select;
+        }
+    }
+}
